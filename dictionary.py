a9fab28538ea37cfa176ea7366331b51db5503bf
sfsrd <sf.srd.31@gmail.com>
2022-02-07 01:54:59 +0200

    Proof of concept
    The program generates words from a dictionary of 5-letter words and reduces the dictionary with each attempt in the Wordle game.

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.corpus import words
+
+def get_dictionary():
+    # download the dictionary for the first usage
+    # nltk.download('words')
+
+    dict = words.words()
+    dict = [ word for word in dict if len(word)==5 ]
+    dict =  [word.lower() for word in dict]
+    return dict
+
+dict = get_dictionary()
---
+++ main.py
@@ -0,0 +1,37 @@
+import random
+import dictionary as d
+
+def check_letter_position(word, letter, position):
+    return  word.rfind(letter) == position
+
+def check_letter(word, letter):
+    return letter in word
+
+def generate_word(dictionary):
+    return dictionary[random.randint(0, len(dictionary)-1)]
+
+def play():
+    dictionary = d.get_dictionary()
+    print('Lets play')
+    word_gen = generate_word(dictionary)
+    print('Generated word: ', word_gen)
+    
+    while True:
+        word_gen = input('Enter word: ')
+        flags = input('Enter flags: ')
+        i = 0
+        for flag in flags:
+            # g - green - is in the word in this position
+            # y - yellow - is in the word, wrong position
+            # r - red - is not in the word
+            if flag == 'g':
+                dictionary = [ word for word in dictionary if check_letter_position(word, word_gen[i], i) ]
+            if flag == 'y':
+                dictionary = [ word for word in dictionary if (check_letter(word, word_gen[i])) ]
+                dictionary = [ word for word in dictionary if not(check_letter_position(word, word_gen[i], i)) ]
+            if flag == 'r':
+                dictionary = [ word for word in dictionary if not(check_letter(word, word_gen[i]))]
+            i = i+1
+        print('Try this: ', dictionary)
+    
+play()
